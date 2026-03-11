11940fbf25be0dd39d167a82dc71edd1324ed9d4
reyhanasta <astareyhan@gmail.com>
2026-03-11 14:06:02 +0700

    update nama dan logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -2,7 +2,7 @@ export default function AppLogo() {
     return (
         <>
             <div className="flex aspect-square size-8 items-center justify-center rounded-md text-sidebar-primary-foreground">
-                <img src="/favicon.svg" alt="App Logo" className="size-6" />
+                <img src="/favicon.svg" alt="App Logo" className="size-8" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
