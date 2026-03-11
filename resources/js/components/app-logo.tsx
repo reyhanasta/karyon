3881dafa55bf25716e87290f26c55cb2448ae16f
reyhanasta <astareyhan@gmail.com>
2026-03-11 14:02:04 +0700

    chore: update build assets

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -6,7 +6,7 @@ export default function AppLogo() {
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
-                    KaryaOne
+                    Mployee
                 </span>
                 <span className="mb-0.5 truncate text-xs leading-tight">
                     Manajemen Kepegawaian
